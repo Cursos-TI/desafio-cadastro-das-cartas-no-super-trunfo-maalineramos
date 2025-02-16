9ac0559aae9b463d7d04a8a26ed18eb6dbf30ce5
maalineramos <ma.alineramos@gmail.com>
2025-02-16 23:33:49 +0000

    add uma biblioteca

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
